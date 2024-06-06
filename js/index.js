e5cba0de4cc6ef763a93db953e44630f08f27344
XinnChan <98129742+XinnChan@users.noreply.github.com>
2024-06-07 02:34:35 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something'.split('')
+const text = '🥰Miya... Xin Punya Sesuatu Tolong Di Buka🥰'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
